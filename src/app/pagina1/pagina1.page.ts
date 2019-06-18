87e5becfb4dd2cc2a4fa20a0872b54c49b4a21c2
Aluno <gustavooguidetti@gmail.com>
2019-06-18 11:35:03 -0300

    angelo corno

--- src/app/pagina1/pagina1.page.ts
+++ src/app/pagina1/pagina1.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pagina2Page } from '../pagina2/pagina2.page';
 import { AngularFireDatabase } from '@angular/fire/database';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina1',
@@ -11,13 +11,15 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class Pagina1Page implements OnInit {
 
 contato: Pagina2Page = new Pagina2Page();
-  constructor(private banco: AngularFireDatabase) { }
+  constructor(private banco: AngularFireDatabase,
+  private rota: Router) { }
 
   ngOnInit() {
   }
 salvar(){
   this.banco.list('contato').push(this.contato);
   this.contato = new Pagina2Page();
+  this.rota.navigate(['pagina2'])
 
 }
 }
